a77036db5b594016542562d64615f01de64e82c3
mac <mac@mac-PC>
2019-07-15 20:52:40 +0800

    login&logout with Cookies

---
+++ cookies practice/src/com/abc/java/LoginServlet.java
@@ -0,0 +1,57 @@
+package com.abc.java;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		request.getRequestDispatcher("link.html").include(request, response);
+		String name =request.getParameter("name");
+		String password =request.getParameter("password");
+		if(password.equals("775299")) {
+			out.print("<br>You are successfully logged in");
+			out.print("<br>Welcome,"+name);
+			Cookie ck =new Cookie("uname",name);
+			response.addCookie(ck);
+		}else {
+			out.print("sorry,username or password error");
+			
+		}
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
