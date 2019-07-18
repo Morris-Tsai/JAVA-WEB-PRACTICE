f3807d4f56df45e50bc195e845886c395367c681
mac <mac@mac-PC>
2019-07-18 22:51:15 +0800

    利用HttpSessionListener計算總登錄和當前登錄用戶

---
+++ Total&Current/src/com/abc/firstservlet/Firstservlet.java
@@ -0,0 +1,56 @@
+package com.abc.firstservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Firstservlet
+ */
+@WebServlet("/Firstservlet")
+public class Firstservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Firstservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String n =request.getParameter("username");
+		out.print("welcome"+n);
+		HttpSession session =request.getSession();
+		session.setAttribute("uname",n);
+		ServletContext ctx =getServletContext();
+		int t=(Integer)ctx.getAttribute("totalusers");
+		int c=(Integer)ctx.getAttribute("currentusers");
+		out.print("<br>total users="+t);
+		out.print("<br>current users="+c);
+		out.print("<br><a href='LogoutServlet'>logout</a>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Total&Current/src/com/abc/listener/CountUserListener.java
@@ -0,0 +1,41 @@
+package com.abc.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class CountUserListener
+ *
+ */
+@WebListener
+public class CountUserListener implements HttpSessionListener {
+	ServletContext ctx =null;
+	static int total=0,current=0;
+
+    public CountUserListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent e)  { 
+         total++;
+         current++;
+         ctx=e.getSession().getServletContext();
+         ctx.setAttribute("totalusers", total);
+         ctx.setAttribute("currentusers", current);
+         
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent e)  { 
+         current--;
+         ctx.setAttribute("currentusers", current);
+    }
+	
+}
